89ae739b7cad59687e2878a8bb9a1c34ed7ce3dd
Tobias Noba <tobiass.nobafioriti@gmail.com>
2026-05-13 16:17:35 -0300

    Agregue mi imagen y la volvi redonda

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,9 +1,10 @@
 import './home.css';
+import FotoCV from '../../../img/FotoCV.png';
 
 export function Home(){
     return(
         <>
-        <img src="" alt="Imagen de Tobias Noba" />
+        <img className='fotoTobiasNoba' src={FotoCV} alt="Foto de Tobias Noba"/>
         <h1>¡Hola, soy Tobias Noba!</h1>
         <h4>Tecnico en informatica personal y profesional</h4>
         <h4>Tecnico superior en desarrollo de software</h4>
--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -3,11 +3,13 @@ import './navBar.css';
 export function NavBar(){
     return(
         <>
-        <h1>Tobias Noba</h1>
-        <h3>Home</h3>
-        <h3>Sobre mi</h3>
-        <h3>Proyectos</h3>
-        <h3>Contactos</h3>
+        <div className='navbar'>
+            <h1>Tobias Noba</h1>
+            <h3>Home</h3>
+            <h3>Sobre mi</h3>
+            <h3>Proyectos</h3>
+            <h3>Contactos</h3>
+        </div>
         </>
     )
 }
