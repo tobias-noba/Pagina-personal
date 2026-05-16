43551f95e8fd01402a5c2dd379faa75230bb6bfb
Tobias Noba <tobiass.nobafioriti@gmail.com>
2026-05-16 10:43:45 -0300

    Agregue mi imagen y la volvi redonda

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -4,11 +4,21 @@ import FotoCV from '../../../img/FotoCV.png';
 export function Home(){
     return(
         <>
-        <img className='fotoTobiasNoba' src={FotoCV} alt="Foto de Tobias Noba"/>
-        <h1>¡Hola, soy Tobias Noba!</h1>
-        <h4>Tecnico en informatica personal y profesional</h4>
-        <h4>Tecnico superior en desarrollo de software</h4>
-        <button>Sobre mi</button>
+            <div className='homeMain'>
+                
+                <div className='homeFoto'>
+
+                <img className='fotoTobiasNoba' src={FotoCV} alt="Foto de Tobias Noba"/>
+                </div>
+
+                <div className='homeText'>
+
+                    <h1>¡Hola, soy Tobias Noba!</h1>
+                    <h4>Tecnico en informatica personal y profesional</h4>
+                    <h4>Tecnico superior en desarrollo de software</h4>
+                    <button>Sobre mi</button>
+                </div>
+            </div>
         </>
     )
 }
--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -3,13 +3,17 @@ import './navBar.css';
 export function NavBar(){
     return(
         <>
-        <div className='navbar'>
-            <h1>Tobias Noba</h1>
-            <h3>Home</h3>
-            <h3>Sobre mi</h3>
-            <h3>Proyectos</h3>
-            <h3>Contactos</h3>
-        </div>
+            <div className='navBar'>
+                <div className='navBarLeft'>
+                    <h1>Tobias Noba</h1>
+                </div>
+                <div className='navBarRight'>
+                    <h3>Home</h3>
+                    <h3>Sobre mi</h3>
+                    <h3>Proyectos</h3>
+                    <h3>Contactos</h3>
+                </div>
+            </div>
         </>
     )
 }
