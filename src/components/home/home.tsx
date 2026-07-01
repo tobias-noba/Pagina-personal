04df140b64706beceac50351144c9935f4c5bf69
Tobias Noba <tobiass.nobafioriti@gmail.com>
2026-07-01 16:26:05 -0300

    Avance con el componente principal y termine de acomodar los botones de LinkedIn, GitHub y el de Descargar CV

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,6 +1,6 @@
 import './home.css';
 import FotoCV from '../../../img/FotoCV.png';
-import {FaGithub, FaLinkedin} from "react-icons/fa";
+import {FaGithub, FaLinkedin, FaDownload} from "react-icons/fa";
 
 export function Home(){
     return(
@@ -21,7 +21,7 @@ export function Home(){
                     <div className='divBotones'>
                         <button className='botonLinkedIn'><FaLinkedin className='iconos'></FaLinkedin></button>
                         <button className='botonGitHub'><FaGithub className='iconos'></FaGithub></button>
-                        <button className='botonDescargarCV'>Descargar CV</button>
+                        <button className='botonDescargarCV'><FaDownload className='iconoDescarga'></FaDownload>Descargar CV</button>
                     </div>
                 </div>
             </div>
